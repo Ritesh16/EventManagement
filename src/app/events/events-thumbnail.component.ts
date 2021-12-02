97827ebff30cd0359b679744ee431c41bc123867
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-12-02 18:32:31 -0500

    Hiding content

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -2,8 +2,24 @@ import  { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'events-thumbnail',
-    templateUrl: './events-thumbnail.component.html'
+    templateUrl: './events-thumbnail.component.html',
+    styles: [
+        `
+          .green {color: #003300 !important; }
+          .bold { font-weight: bold; }
+          .thumbnail { min-height: 210px; }
+          .pad-left { margin-left: 10px; }
+          .well div { color: #bbb; }
+        `
+    ]
 })
 export class EventsThumbnailComponent {
    @Input() event:any;
+
+   getStartTimeClass() {
+       if(this.event && this.event.time === '8:00 am')
+          return ['green','bold'];
+
+        return [];
+   }
 }
