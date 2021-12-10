5b06ac296949fba22b5349909504a4e24556950b
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-12-10 10:38:28 -0500

    Adding sorting and filtering of sessions

--- src/app/events-details/events-details.component.ts
+++ src/app/events-details/events-details.component.ts
@@ -11,6 +11,8 @@ import { EventService } from '../_services/event.service';
 export class EventsDetailsComponent implements OnInit {
  event: any;
  addMode: boolean;
+ filterBy: string = 'all';
+ sortBy: string = 'votes';
 
   constructor(private eventService : EventService, private route: ActivatedRoute) { }
 
--- src/app/session-list/session-list.component.ts
+++ src/app/session-list/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { ISession } from '../_models/event.model';
 
 @Component({
@@ -6,12 +6,41 @@ import { ISession } from '../_models/event.model';
   templateUrl: './session-list.component.html',
   styleUrls: ['./session-list.component.css']
 })
-export class SessionListComponent implements OnInit {
+export class SessionListComponent implements OnChanges {
  @Input() sessions: ISession[];
+ @Input() filterBy: string;
+ @Input() sortBy: string;
+ visibleSessions: ISession[];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    if(this.sessions) {
+      this.filterSessions(this.filterBy);
+
+      this.sortBy == 'name' ? this.visibleSessions.sort(sortByNameAsc) :
+              this.visibleSessions.sort(sortByVotesAsc);
+    }
   }
 
+  filterSessions(filterBy: string) {
+    if(filterBy === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    } else{
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filterBy;
+      });
+    }
+  }
+
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession){
+  if(s1.name > s2.name) return 1;
+  else if(s1.name === s2.name) return 0;
+  else return -1;
+}
+
+function sortByVotesAsc(s1: ISession, s2: ISession){
+  return s2.voters.length - s1.voters.length;
 }
